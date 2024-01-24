17e4f4c643f4ccd6ca0bd1c022eb5844d6378bd4
Kemal Gencay <kemalgencay@gmail.com>
2024-01-24 21:01:00 +0300

    commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,10 +3,10 @@ import Footer from "./Footer.tsx";
 
 export default function layouts({ children }: any) {
   return (
-    <div>
+    <div class="container mx-auto px-4">
       <img src="/header1.jpg" />
 
-      <nav class="tw w-full px-4 py-3 bg-orange-800 text-white mb-10">
+      <nav class="tw w-full px-4 py-3 bg-blue-800 text-white mb-10">
         <a class="tw mx-4" href="/">Ana sayfa</a>
         <a class="tw mx-4" href="/hakkimda">Hakkımda</a>
         <a class="tw mx-4" href="https://kemalblog.com" target="blank">
